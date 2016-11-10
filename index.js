dd85f44185c0d4565b94909d5ea15e1fd8e45d8d
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-11-10 18:47:23 +0100

    Add pacapt paramaeters object + Create pacapt function

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 
 const spawn = require('child_process').spawn;
-const pacapt = spawn(__dirname + '/pacapt/pacapt', ['--version']);
 
-pacapt.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
+const opts = {
+  Q: '-Q', Qc: '-Qc', Qi: '-Qi', Ql: '-Ql', Qm: '-Qm', Qo: '-Qo', Qp: '-Qp', Qs: '-Qs', Qu: '-Qu',
+  R: '-R', Rn: '-Rn', Rns: '-Rns', Rs: '-Rs',
+  S: '-S', Sc: '-Sc', Scc: '-Scc', Sccc: '-Sccc', Si: '-Si', Sii: '-Sii', Sl: '-Sl', Ss: '-Ss', Su: '-Su', Suy: '-Suy', Sw: '-Sw', Sy: '-Sy',
+  U: '-U'
+};
 
-pacapt.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-});
+function execPacapt(args) {
+  const pacapt = spawn(__dirname + '/pacapt/pacapt', args);
 
-pacapt.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
+  pacapt.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+  });
+
+  pacapt.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+  });
+
+  pacapt.on('close', (code) => {
+      console.log(`pacapt exited status ${code}`);
+  });
+}
+
+execPacapt([opts.S, 'git']); // fails, wait for user input
