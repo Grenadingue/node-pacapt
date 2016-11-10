3d7449ff19e605e1e58849fae050fcad5397c5ef
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-11-10 17:59:12 +0100

    Add sample pacapt process spawning

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+const spawn = require('child_process').spawn;
+const pacapt = spawn(__dirname + '/pacapt/pacapt', ['--version']);
+
+pacapt.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+pacapt.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+
+pacapt.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
