5a01100f700933c2fcdbea24281e1e8b2f38bb3a
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-11-11 17:14:11 +0100

    Export module data and functions + Update options

--- index.js
+++ index.js
@@ -6,23 +6,26 @@ const opts = {
   Q: '-Q', Qc: '-Qc', Qi: '-Qi', Ql: '-Ql', Qm: '-Qm', Qo: '-Qo', Qp: '-Qp', Qs: '-Qs', Qu: '-Qu',
   R: '-R', Rn: '-Rn', Rns: '-Rns', Rs: '-Rs',
   S: '-S', Sc: '-Sc', Scc: '-Scc', Sccc: '-Sccc', Si: '-Si', Sii: '-Sii', Sl: '-Sl', Ss: '-Ss', Su: '-Su', Suy: '-Suy', Sw: '-Sw', Sy: '-Sy',
-  U: '-U'
+  U: '-U',
+  noConfirm: '--noconfirm'
 };
 
 function execPacapt(args) {
+  console.log(__dirname + '/pacapt/pacapt', args);
   const pacapt = spawn(__dirname + '/pacapt/pacapt', args);
 
   pacapt.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+    console.log(`stdout: ${data}`);
   });
 
   pacapt.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
+    console.log(`stderr: ${data}`);
   });
 
   pacapt.on('close', (code) => {
-      console.log(`pacapt exited status ${code}`);
+    console.log(`pacapt exited status ${code}`);
   });
 }
 
-execPacapt([opts.S, 'git']); // fails, wait for user input
+module.exports.opts = opts;
+module.exports.exec = execPacapt;
