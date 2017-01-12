cf739afe7d145aa033b415e69564492b756cf08f
Nicolas Chauvin <chauvin.nico@gmail.com>
2017-01-12 17:45:01 +0100

    Update example script according to new version

--- example.js
+++ example.js
@@ -1,61 +1,64 @@
-const pacapt = require('./index');
+const pacapt = require('./index.js');
 
-console.log(`Removing cmake...`);
-pacapt.remove(['cmake'])
-  .then((output) => {
-    console.log(`Cmake removed !`);
+function executeOperationIfImplemented(operation, args) {
+  return new Promise((fulfill, reject) => {
+    if (pacapt.localInfos.availableOperations[operation]) {
+      pacapt[operation](args).then((output) => {
+        fulfill(output);
+      }).catch((output) => {
+        reject(output);
+      });
+    } else {
+      fulfill('Operation \'' + operation + '\' not implemented');
+    }
+  });
+}
+
+function updateDatabaseIfImplemented() {
+  return executeOperationIfImplemented('Sy', []);
+}
+
+pacapt.init().then(() => {
+  console.log('Local infos:');
+  console.log(pacapt.localInfos);
+
+  console.log('Installing vlc...');
+  pacapt.install(['vlc']).then((output) => {
+    console.log('vlc installed!');
     console.log(output);
 
-    console.log(`Updating software database...`);
-    pacapt.updateDatabase().then((output) => {
-      console.log(`Software database updated!`);
-      console.log(output);
-
-      console.log(`Re-installing cmake...`);
-      pacapt.install(['cmake']).then((output) => {
-        console.log(`Cmake re-installed!`);
+    console.log('Removing vlc...');
+    pacapt.remove(['vlc'])
+      .then((output) => {
+        console.log('vlc removed !');
         console.log(output);
 
-      }).catch((error) => {
-        console.log(`Error during cmake install`);
-        console.log(error);
-      });
-    }).catch((error) => {
-      console.log(`Error during package database update`);
-      console.log(error);
-    });
-  }).catch((error) => {
-    console.log(`Error during cmake removal`);
-    console.log(error);
-  });
-
-  console.log(`Trying to start a new pacapt instance at the same time`);
-
-  console.log(`2 Removing cmake...`);
-  pacapt.remove(['cmake'])
-    .then((output) => {
-      console.log(`2 Cmake removed !`);
-      console.log(output);
-
-      console.log(`2 Updating software database...`);
-      pacapt.updateDatabase().then((output) => {
-        console.log(`2 Software database updated!`);
-        console.log(output);
-
-        console.log(`2 Re-installing cmake...`);
-        pacapt.install(['cmake']).then((output) => {
-          console.log(`2 Cmake re-installed!`);
+        console.log('Updating software database...');
+        updateDatabaseIfImplemented().then((output) => {
+          console.log('Software database updated (or ignored)!');
           console.log(output);
 
+          console.log('Re-installing vlc...');
+          pacapt.install(['vlc']).then((output) => {
+            console.log('vlc re-installed!');
+            console.log(output);
+
+          }).catch((error) => {
+            console.log('Error during vlc install');
+            console.log(error);
+          });
         }).catch((error) => {
-          console.log(`2 Error during cmake install`);
+          console.log('Error during package database update');
           console.log(error);
         });
       }).catch((error) => {
-        console.log(`2 Error during package database update`);
+        console.log('Error during vlc removal');
         console.log(error);
       });
-    }).catch((error) => {
-      console.log(`2 Error during cmake removal`);
-      console.log(error);
-    });
+  }).catch((error) => {
+    console.log('Error during vlc install');
+    console.log(error);
+  });
+}).catch(() => {
+  console.log('Error during pacapt initialization');
+});
